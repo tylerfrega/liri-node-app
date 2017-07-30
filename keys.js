2db94dc6156063fc2f9d65162602d0a4343c01cc
Tyler J. Frega <tyfrega@gmail.com>
2017-07-29 22:20:46 -0400

    progress

--- keys.js
+++ keys.js
@@ -7,3 +7,8 @@ exports.twitterKeys = {
   access_token_key: '891402530806169600-akFLzlroMUY47Dy3VCnJVCkmBsbRM5w',
   access_token_secret: 'Iix53nOaKPBzVMnTddbxc1Ya6VVeAZXqwKtDo9p5rQXlw',
 }
+
+exports.spotifyKeys = {
+    id: '51db06a734ec44f2996174747c7bd10c',
+    secret: '24291cb12daa4b529037d4614a905937'
+}
--- liri.js
+++ liri.js
@@ -6,6 +6,13 @@ switch(command){
     case 'my-tweets':
       getTweets();
       break;
+    case 'spotify-this-song':
+      searchSpotify();
+      break;
+    case 'movie-this':
+      searchOMDB();
+    break;
+
 
 }
 
@@ -22,5 +29,55 @@ function getTweets(){
   }else{
       console.log(error);
   }
+ });
+};
+
+
+function searchSpotify(){
+    var Spotify = require('node-spotify-api');
+    var client = new Spotify(keys.spotifyKeys);
+
+    client.search({ type: 'track', query: search }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+
+ for(i=0; i<data.tracks.items.length; i++){
+
+    var artist = data.tracks.items[i].artists[0].name;
+    var songName = data.tracks.items[i].name;
+    var preview_url = data.tracks.items[i].preview_url;
+    var albumName = data.tracks.items[i].album.name
+    
+    console.log(artist, songName, preview_url, albumName);
+  } 
+  
 });
 }
+
+
+function searchOMDB(){
+    var request = require("request");
+    var movieName = search;
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+   
+
+    request(queryUrl, function(error, response, body){
+        var title = JSON.parse(body).Title;
+        var year = JSON.parse(body).Year;
+        var imdbRating = JSON.parse(body).Ratings.imdbRating;
+        var country = JSON.parse(body).Country;
+        var language = JSON.parse(body).Language;
+        var plot = JSON.parse(body).Plot;
+        var actors = JSON.parse(body).Actors;
+        
+        if(!error && response.statusCose === 200);{
+           // console.log(title, year, imdbRating, country, language, plot, actors);
+           console.log(title, year, imdbRating, country, language, plot, actors)
+        }
+    })
+}
+
+
+    
